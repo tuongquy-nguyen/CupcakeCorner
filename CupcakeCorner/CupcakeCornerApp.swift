3c68157672fe6e81ad27610a5eaf3463ea0f1cc8
Nguyen Tuong Quy <tuongquynguyen.159@gmail.com>
2022-04-25 08:58:23 +0700

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by KET on 25/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
